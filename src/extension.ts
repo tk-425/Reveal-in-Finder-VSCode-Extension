55e15b23a58063b13a9a12a969acd378cfaec72e
trry <54043933+tk-425@users.noreply.github.com>
2025-04-27 17:05:06 -0400

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,28 @@
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    'reveal-in-finder.getFilePath',
+    () => {
+      const activeEditor = vscode.window.activeTextEditor;
+
+      if (!activeEditor) {
+        vscode.window.showErrorMessage('No active text editor found!');
+        return;
+      }
+
+      // Full system file path
+      const filePath = activeEditor.document.uri.fsPath;
+
+      // Open the file's directory in Finder
+      
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
