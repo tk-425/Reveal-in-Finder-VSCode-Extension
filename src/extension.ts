9e71633c649811a1ea8b525bcd6e0bdc4715a952
trry <54043933+tk-425@users.noreply.github.com>
2025-04-27 18:30:11 -0400

    add opening current editor window's folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
+import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'reveal-in-finder.getFilePath',
-    () => {
+    async () => {
       const activeEditor = vscode.window.activeTextEditor;
 
       if (!activeEditor) {
@@ -17,7 +19,13 @@ export function activate(context: vscode.ExtensionContext) {
       const filePath = activeEditor.document.uri.fsPath;
 
       // Open the file's directory in Finder
-      
+      childProcess.exec(`open -R "${filePath}"`, (error) => {
+        if (error) {
+          vscode.window.showErrorMessage(
+            `Failed to open Finder: ${error.message}`
+          );
+        }
+      });
     }
   );
 
